861794960fe5fa1f3c23ed42b0db22ae1fbef793
loidt132 <tienloi2007@gmail.com>
2026-03-23 23:08:43 +0700

    update vite config

--- nhatro-unified/web/vite.config.js
+++ nhatro-unified/web/vite.config.js
@@ -1,15 +1,15 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
- 
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      // các request bắt đầu bằng /api sẽ được proxy sang API
-      '/api': {
-        target: 'https://loidt132-nhatro.vercel.app',
-        changeOrigin: true,
-      }
-    }
-  }
-});
+  plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://loidt132-nhatro.vercel.app',
+        changeOrigin: true,
+        secure: true
+      }
+    }
+  }
+})
