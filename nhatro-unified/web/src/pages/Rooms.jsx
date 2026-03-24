ea1428bdb232a753aab1dbcabce5bfa8d863ac4e
loidt132 <tienloi2007@gmail.com>
2026-03-24 23:34:47 +0700

    update roooms

--- nhatro-unified/web/src/pages/Rooms.jsx
+++ nhatro-unified/web/src/pages/Rooms.jsx
@@ -10,30 +10,18 @@ import { loadState, saveState, monthKey, currency, uid, calcTotals } from '../ut
 import Footer from '../components/Footer';
 
 export default function Rooms(){
-  //const [state, setState] = useState(loadState());
+   
   //const [state, setState] = useState(null);
-  //const [state, setState] = useState(() => loadState());
-  /*useEffect(() => {
-    const refresh = () => setState(loadState());
+  const [state, setState] = useState(() => loadState());
+  useEffect(() => {
+    const refresh = () => {
+      const newState = loadState();
+      setState(newState); // React tự tối ưu nếu state không đổi
+    };
+  
     window.addEventListener('boarding_state_updated', refresh);
     return () => window.removeEventListener('boarding_state_updated', refresh);
   }, []);
-  if (!state) return <div className="p-6">Đang tải dữ liệu...</div>;
-  */
-  const [state, setState] = useState(() => loadState());
-
-useEffect(() => {
-const refresh = () => {
-const newState = loadState();
-setState(prev =>
-JSON.stringify(prev) === JSON.stringify(newState) ? prev : newState
-);
-};
-
-window.addEventListener('boarding_state_updated', refresh);
-return () => window.removeEventListener('boarding_state_updated', refresh);
-
-}, []);
 
   const [month, setMonth] = useState(monthKey());
   const { rooms, tenants, readings, invoices, payments } = state;
