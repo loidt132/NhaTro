6044423441407f76a518ac635b931eb746afee56
loidt132 <tienloi2007@gmail.com>
2026-03-26 23:53:02 +0700

    add hydrate

--- nhatro-unified/web/src/main.jsx
+++ nhatro-unified/web/src/main.jsx
@@ -5,5 +5,17 @@ import App from './App';
 import './index.css';
 import { hydrateState } from './utils/state';
 
-await hydrateState(); // CHẶN Ở ĐÂY
-ReactDOM.createRoot(document.getElementById('root')).render(<React.StrictMode><App/></React.StrictMode>);
+//await hydrateState(); // CHẶN Ở ĐÂY
+//ReactDOM.createRoot(document.getElementById('root')).render(<React.StrictMode><App/></React.StrictMode>);
+
+async function bootstrap() {
+  await hydrateState(); // ✅ hợp lệ
+
+  ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
+
+bootstrap();
